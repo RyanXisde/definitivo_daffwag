796184e2a7be62cdf4d06c9a6e9b0fe82f00f353
juh <juliana.karling@escola.pr.gov.br>
2026-07-02 10:41:38 -0300

    Modify funcionario table and insert sample records
    Updated table structure and added sample data for funcionarios.

--- funcionario.sql
+++ funcionario.sql
@@ -1,15 +1,18 @@
 CREATE TABLE IF NOT EXISTS funcionario(
-  id_seq INTEGER PRIMARY KEY AUTOINCREMENT,
-  nome TEXT NOT NULL,
-  area TEXT NOT NULL,
-  telefone TEXT NOT NULL,
-  cpf TEXT NOT NULL UNIQUE, 
-  data DATE NOT NULL,
-  salario REAL NOT NULL
-  );
-  
-  INSERT INTO funcionario(nome, area, telefone, cpf, data, salario)
-  VALUES
-  ('Ricardo Pereira', 'Stock', '45991450968', '120.780.829-61', 2025, 1600);
+    id_seq INTEGER PRIMARY KEY AUTOINCREMENT,
+    Nome TEXT NOT NULL,
+    Area TEXT NOT NULL,
+    Telefone TEXT NOT NULL,
+    Cpf TEXT NOT NULL UNIQUE,
+    Data TEXT NOT NULL,
+    Salario REAL NOT NULL
+);
+
+INSERT INTO funcionario
+(Nome, Area, Telefone, Cpf, Data, Salario)
+VALUES
+('Ricardo Pereira', 'Estoquista', '41999998888', '120.780.829-61', '2025-01-15', 1600),
+('David Rodrigues', 'Operador de Caixa', '4129323263', '816.758.097-04', '2020-05-20', 2000),
+('Flavia Silva', 'Vendedora', '4128794204', '548.416.963-18', '2026-03-10', 1650);
   
   SELECT * FROM funcionario
